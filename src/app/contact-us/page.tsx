d246ba6ba0977d695d75d453e0201bb8311d51d9
Himanshu Srivastava <himanshu20032002@gmail.com>
2025-07-07 21:37:55 +0530

    faq tiles made

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,16 +1,38 @@
+"use client";
+
 import React from "react";
 import Banner from "../../components/banner/Banner";
 import { BannerPage } from "../../components/banner/Banner";
 import Form from "@/components/contact/Form";
+import { useState } from "react";
 
-const getFAQTile = (heading: string, content: string) => {
+const FAQTile = ({
+  heading,
+  content,
+}: {
+  heading: string;
+  content: string;
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className="bg-[#F5F5F5] rounded-[16px] p-6 flex flex-col gap-6">
       <div className="flex justify-between font-bold">
         <div className="text-[#151515] max-w-[80%]">{heading}</div>
-        <div>x</div>
+        <div className="flex items-start justify-center">
+          <button
+            onClick={toggleExpanded}
+            className="text-[#151515] cursor-pointer text-2xl transition-opacity"
+          >
+            {isExpanded ? "×" : "+"}
+          </button>
+        </div>
       </div>
-      <div className="text-[#747474]">{content}</div>
+      {isExpanded && <div className="text-[#747474]">{content}</div>}
     </div>
   );
 };
@@ -25,13 +47,51 @@ const Page = () => {
             <div className="text-h1 font-bold leading-[120%] mb-16">
               Frequently asked questions
             </div>
-            {getFAQTile(
-              "Do you offer same-day or next-day delivery?Do you offer same-day or next-day delivery?Do you offer same-day or next-day delivery?",
-              "Yes, same-day or next-day delivery is available for select locations, based on stock availability."
-            )}
+            <div className="flex flex-col gap-4">
+              <FAQTile
+                heading="Do you offer same-day or next-day delivery?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="What areas do you currently distribute to?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="Where are your offices or warehouses located?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="How long does it take to receive a response after I contact you?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="Who can I speak to for business partnership inquiries?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="What forms of payment do you accept?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="Can I request a product catalog?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="Are you an authorised distributor?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="Do you deal in temperature-sensitive or cold-chain products?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+              <FAQTile
+                heading="Are all your products certified and compliant?"
+                content="Yes, same-day or next-day delivery is available for select locations, based on stock availability."
+              />
+            </div>
           </div>
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2 mt-34">
           <div className="flex flex-col gap-4">
             <div className="mb-6 text-left pt-10 pr-10">
               <div className="text-body-1">+91 9832021150</div>
